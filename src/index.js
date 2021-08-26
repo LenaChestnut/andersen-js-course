7b595e697fb77d2434a088be02b00fc21646c422
LenaChestnut <lenachestnut@gmail.com>
2021-08-26 21:27:33 +0300

    Create an array of unique values

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@
 import any from './array-practice/task1';
 import arrayDiff from './array-practice/task2';
 import forEachRight from './array-practice/task3';
+import union from './array-practice/task4';
 
 import { task1Old, task1New } from './es-features/task1';
 import { task2Old, task2New } from './es-features/task2';
@@ -125,3 +126,7 @@ console.log(arrayDiff([1, 3, 3, 4], [1, 3, '4'])); // [4, '4']
 console.log('=======TASK 3======');
 
 forEachRight([1, 2, 3, 4], val => console.log(val));
+
+console.log('=======TASK 4======');
+
+console.log(union([5, 1, 2, 3, 3], [4, 3, 2])); // [5, 1, 2, 3, 4]
